b2fe7ebaa6cd84d8269eaf2e8f0ddab550103f4c
Federico D'Angelo <federicodangelo@gmail.com>
2026-03-07 19:26:37 -0300

    split classes into their own files

--- Engine.Network/Client/ClientNetworkManager.cs
+++ Engine.Network/Client/ClientNetworkManager.cs
@@ -222,28 +222,3 @@ public sealed class ClientNetworkManager : IDisposable
         }
     }
 }
-
-/// <summary>
-/// Represents a remote player as seen by the client.
-/// </summary>
-public class RemotePlayer
-{
-    public byte PlayerId { get; }
-    public string Name { get; }
-
-    public NetPlayerLocation Location;
-    public NetPlayerState State;
-    public NetPlayerInfo Info;
-
-    /// <summary>True once at least one state update has been received.</summary>
-    public bool HasReceivedState;
-
-    public RemotePlayer(byte playerId, string name, NetPlayerLocation location, NetPlayerInfo info, NetPlayerState state)
-    {
-        PlayerId = playerId;
-        Name = name;
-        Location = location;
-        Info = info;
-        State = state;
-    }
-}
---
+++ Engine.Network/Client/RemotePlayer.cs
@@ -0,0 +1,26 @@
+namespace Engine.Network.Client;
+
+/// <summary>
+/// Represents a remote player as seen by the client.
+/// </summary>
+public class RemotePlayer
+{
+    public byte PlayerId { get; }
+    public string Name { get; }
+
+    public NetPlayerLocation Location;
+    public NetPlayerState State;
+    public NetPlayerInfo Info;
+
+    /// <summary>True once at least one state update has been received.</summary>
+    public bool HasReceivedState;
+
+    public RemotePlayer(byte playerId, string name, NetPlayerLocation location, NetPlayerInfo info, NetPlayerState state)
+    {
+        PlayerId = playerId;
+        Name = name;
+        Location = location;
+        Info = info;
+        State = state;
+    }
+}
